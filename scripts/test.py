362d72fb1401e0124ed6c300d521d4f09a203c30
deepakhr1999 <deepakhr1999@gmail.com>
2020-12-15 12:45:45 +0530

    test.main can take loader from outside

--- scripts/test.py
+++ scripts/test.py
@@ -14,7 +14,7 @@ import sys
 from data.dataset import NERDataset
 from models.networks import GlobalContextualDeepTransition
 from scripts.beamsearch import BeamSearcher
-import conlleval_perl as conll
+import scripts.conlleval_perl as conll
 
 def getParser():
     parser = argparse.ArgumentParser()
@@ -48,25 +48,33 @@ def getParser():
     return parser
 
 
-
-def main(args):
-    if args.notebook:
-        from tqdm.notebook import tqdm
-    else:
-        from tqdm import tqdm
-    # load dataset
+def getLoader(args):
     sourceName  = os.path.join(args.root, 'eng.train.src')
     targetName  = os.path.join(args.root, 'eng.train.trg')
     gloveFile   = os.path.join(args.root, 'trimmed.300d.Cased.txt')
     symbFile    = os.path.join(args.root, 'sym.glove')
     testSrc     = os.path.join(args.root, f'eng.{args.type}.src')
     testTrg     = os.path.join(args.root, f'eng.{args.type}.trg')
-    predFile    = "preds.txt"
-    beamSize    = args.beam
     data = NERDataset(sourceName, targetName, gloveFile, symbFile)
     data.readTestFile(testSrc, testTrg)
     loader = data.getLoader(4096, shuffle=False)
+    return loader, data.tags
 
+def main(args, loader=None, tags=None):
+    if args.notebook:
+        from tqdm.notebook import tqdm
+    else:
+        from tqdm import tqdm
+    
+    # load dataset
+    if not loader:
+        loader, tags = getLoader(args)
+
+    testSrc     = os.path.join(args.root, f'eng.{args.type}.src')
+    testTrg     = os.path.join(args.root, f'eng.{args.type}.trg')
+    
+    predFile    = "preds.txt"
+    beamSize    = args.beam
 
     # load model
     prevCheckpointPath = args.ckpt
@@ -89,7 +97,7 @@ def main(args):
         preds = tester(batch)
         out.extend(preds)
 
-    tester.writePreds(out, data.tags, predFile, testSrc)
+    tester.writePreds(out, tags, predFile, testSrc)
     tester.getResultFile(testSrc, predFile, testTrg, args.file)
     acc, prec, rec, f1 = conll.main(args)
     os.remove(predFile)
